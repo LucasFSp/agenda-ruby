284d731af0cfe9595632a2b5f3542bd141982113
Lucas Figueroa Spanholo <68356372+LucasFSp@users.noreply.github.com>
2022-02-11 01:14:27 -0300

    Update agenda.rb

--- agenda.rb
+++ agenda.rb
@@ -1,5 +1,27 @@
-puts 'teste'
 
-puts "teste 2.0"
+@agenda = [
+    {nome: "Diego", telefone: "0000"},
+    {nome: "Rafael", telefone: "22222"}
+]
 
-puts "teste 3"
+def todos_contatos
+    @agenda.each do |contato|
+        puts "#{contato[:nome]} - #{contato[telefone]}"
+    end
+    puts "--------------------------"
+end
+
+loop do
+
+puts "1. Contatos\n2. Adicionar Contatos\n3. Ver contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+    codigo = gets.chomp.to_i
+
+    case
+    when codigo ==0
+        puts "Até mais!"
+        break
+    when codigo == 1
+        todos_contatos
+    end 
+
+end
